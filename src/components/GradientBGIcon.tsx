daa17a44cb4cd6fa30dfe410341d21bf04bfffcf
Karl George <karlgeorge87@gmail.com>
2024-08-13 13:45:15 +0200

    Add gradient menu component

--- src/components/GradientBGIcon.tsx
+++ src/components/GradientBGIcon.tsx
@@ -1,5 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {COLORS, SPACING} from '../theme/theme';
 
 interface GradientBGIconProps {
   name: string;
@@ -9,12 +11,47 @@ interface GradientBGIconProps {
 
 const GradientBGIcon = ({name, color, size}: GradientBGIconProps) => {
   return (
-    <View>
-      <Text>GradientBGIcon</Text>
+    <View style={styles.container}>
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+        style={styles.linearGradientBG}
+        colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}>
+        <View style={styles.icon}>
+          <Text
+            style={{
+              color: 'white',
+            }}>
+            T
+          </Text>
+        </View>
+      </LinearGradient>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 2,
+    borderColor: COLORS.secondaryGreyHex,
+    borderRadius: SPACING.space_12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.secondaryDarkGreyHex,
+    overflow: 'hidden',
+  },
+  linearGradientBG: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    height: 40,
+    width: 40,
+    borderRadius: 50,
+    backgroundColor: COLORS.primaryGreyHex,
+  },
+});
 
 export default GradientBGIcon;
