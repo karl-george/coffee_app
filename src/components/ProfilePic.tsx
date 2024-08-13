430b12e8a6b555bb66cec835dadaf284dcab9d9d
Karl George <karlgeorge87@gmail.com>
2024-08-13 13:45:01 +0200

    Add profile component

--- src/components/ProfilePic.tsx
+++ src/components/ProfilePic.tsx
@@ -1,14 +1,30 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {COLORS, SPACING} from '../theme/theme';
 
 const ProfilePic = () => {
   return (
-    <View>
-      <Text>ProfilePic</Text>
+    <View style={styles.container}>
+      <Text style={styles.avatar}>K</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+    borderRadius: SPACING.space_12,
+    borderWidth: 2,
+    borderColor: COLORS.secondaryDarkGreyHex,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+  avatar: {
+    fontSize: 20,
+    color: COLORS.primaryWhiteHex,
+  },
+});
 
 export default ProfilePic;
