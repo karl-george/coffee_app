dde36432af12c0a52909f3e94a08ecd98241174d
Karl George <karlgeorge87@gmail.com>
2024-08-14 14:09:55 +0200

    Add details screen

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,14 +1,52 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useStore} from '../store/store';
+import {COLORS} from '../theme/theme';
+import ImageBackgroundInfo from '../components/ImageBackgroundInfo';
 
-const DetailsScreen = () => {
+const DetailsScreen = ({navigation, route}: any) => {
+  const itemOfIndex = useStore((state: any) =>
+    route.params.type == 'Coffee' ? state.CoffeeList : state.BeanList,
+  )[route.params.index];
+
+  const backHandler = () => {
+    navigation.goBack();
+  };
+
   return (
-    <View>
-      <Text>DetailsScreen</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollViewFlex}>
+        <ImageBackgroundInfo
+          enableBackHandler={true}
+          imageLink="https://images.unsplash.com/photo-1495774856032-8b90bbb32b32?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          type={route.params.type}
+          id={itemOfIndex.id}
+          favorite={itemOfIndex.favorite}
+          name={itemOfIndex.name}
+          special_ingredient={itemOfIndex.special_ingredient}
+          ingredients={itemOfIndex.ingredients}
+          average_rating={itemOfIndex.average_rating}
+          ratings_count={itemOfIndex.ratings_count}
+          roasted={itemOfIndex.roasted}
+          backHandler={backHandler}
+          toggleFavorite={() => {}}
+        />
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  scrollViewFlex: {
+    flexGrow: 1,
+  },
+});
 
 export default DetailsScreen;
