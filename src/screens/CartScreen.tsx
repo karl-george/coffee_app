4b157e2db57b616ecae046441071395f4187d533
Karl George <karlgeorge87@gmail.com>
2024-08-16 10:25:06 +0200

    Add cart screen

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,7 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useStore} from '../store/store';
 
 const CartScreen = () => {
+  const cartList = useStore((state: any) => state.CartList);
+  console.log(cartList);
+
   return (
     <View>
       <Text>CartScreen</Text>
